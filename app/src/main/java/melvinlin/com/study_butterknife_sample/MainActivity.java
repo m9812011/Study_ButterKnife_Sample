116b12bec91ad3d3276cda06e1d6aa2ac40bfd04
melvin <m9812011@gmail.com>
2019-06-16 08:59:33 +0800

    Initial commit

---
+++ app/src/main/java/melvinlin/com/study_butterknife_sample/MainActivity.java
@@ -0,0 +1,33 @@
+package melvinlin.com.study_butterknife_sample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.tv1)
+    TextView tv1;
+
+    @BindView(R.id.tv2)
+    TextView tv2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.tv1)
+    public void click(View view) {
+        Toast.makeText(this, "toast", Toast.LENGTH_SHORT).show();
+    }
+}
